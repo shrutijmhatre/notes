3055348173d63d307d30cf673f4ce33a07ee1d7a
shrutijmhatre <shrutijmhatre@gmail.com>
2024-03-06 15:53:07 +0530

    add DTO and POST api

---
+++ Notes/Notes.API/Controllers/NotesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Notes.API.Data;
+using Notes.API.Models.DomainModels;
+using Notes.API.Models.DTO;
+
+namespace Notes.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly NotesDbContext dbContext;
+
+        public NotesController(NotesDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        public IActionResult AddNote(AddNoteRequest addNoteRequest)
+        {
+            var note = new Note
+            {
+                Title = addNoteRequest.Title,
+                Description = addNoteRequest.Description,
+                DateCreated = DateTime.Now,
+            };
+
+            dbContext.Notes.Add(note);
+            dbContext.SaveChanges();
+
+            return Ok(note);
+
+        }
+    }
+}
