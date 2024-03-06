7a8866abaf6a379b72a372dac65a61053352bbfc
shrutijmhatre <shrutijmhatre@gmail.com>
2024-03-06 15:38:50 +0530

    Data model and DbContext with migration

---
+++ Notes/Notes.API/Data/NotesDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.API.Models.DomainModels;
+
+namespace Notes.API.Data
+{
+    public class NotesDbContext:DbContext
+    {
+        public NotesDbContext(DbContextOptions options): base(options)
+        {
+        }
+
+        public DbSet<Note> Notes { get; set; }
+    }
+}
---
+++ Notes/Notes.API/Models/DomainModels/Note.cs
@@ -0,0 +1,10 @@
+﻿namespace Notes.API.Models.DomainModels
+{
+    public class Note
+    {
+        public Guid Id { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
